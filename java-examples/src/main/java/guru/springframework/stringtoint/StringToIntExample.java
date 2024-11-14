dceb6bb59d578359e3bea80b3b4db5580392f862
John Thompson <john@springframework.guru>
2024-11-14 16:10:40 -0500

    string to int example

---
+++ java-examples/src/main/java/guru/springframework/stringtoint/StringToIntExample.java
@@ -0,0 +1,12 @@
+package guru.springframework.stringtoint;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public class StringToIntExample {
+    public static void main(String[] args) {
+        String str = "123";
+        int i = Integer.parseInt(str);
+        System.out.println("Converted integer: " + i);
+    }
+}
